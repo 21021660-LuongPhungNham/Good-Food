344a6276d3af7c9c5dbd34dc2dd40d2c719a3893
Nham <100785756+luongphungnham@users.noreply.github.com>
2024-11-21 22:14:54 +0700

    Update database db.js for Backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 //const MONGO_URL = 'mongodb+srv://kainfuon:Test12345678@cluster0.ayrph.mongodb.net/food-del'
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/my_database')
+    await mongoose.connect('mongodb+srv://kainfuon:Test12345678@cluster0.ayrph.mongodb.net/food-del')
         .then(() => {
             console.log('Database is connected');
         })
